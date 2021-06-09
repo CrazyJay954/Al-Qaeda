e2e513d78611614283491177e63ca103dba8d46f
Аркадий Паровозов <arkasha779@hotmail.com>
2021-06-09 11:59:49 +0300

    0

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Al_Qaeda.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
